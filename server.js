c1972e078244beb5ad1c4a6ddd76157b7b52d5a9
mac <mac@MacBook-Pro-de-mac-2.local>
2022-01-31 19:24:28 +0100

    add mongodb

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is
 
 const db = require("./app/models");
 db.mongoose
-  .connect(db.url, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
